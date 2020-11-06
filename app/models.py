a9e0011982ca1e88964dc63f18d4525858ba12c4
forza111 <nikitos.ionkin@yandex.ru>
2020-11-06 13:18:45 +0300

    change followed_posts

--- app/models.py
+++ app/models.py
@@ -25,6 +25,7 @@ class User(UserMixin,db.Model):
                                secondaryjoin = (followers.c.followed_id == id),
                                backref = db.backref('followers', lazy = 'dynamic'), lazy = 'dynamic')
 
+
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
@@ -37,6 +38,25 @@ class User(UserMixin,db.Model):
     def avatar(self, size):
         digest = md5(self.email.lower().encode('utf8')).hexdigest()
         return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(digest, size)
+
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+
+    def is_following(self, user):
+        return self.followed.filter(
+            followers.c.followed_id == user.id).count() > 0
+
+    def followed_posts(self):
+        followed = Post.query.join(
+            followers, (followers.c.followed_id == Post.user_id)).filter(
+            followers.c.follower_id == self.id)
+        own = Post.query.filter_by(user_id=self.id)
+        return followed.union(own).order_by(Post.timestamp.desc()
 
 
 @login.user_loader
