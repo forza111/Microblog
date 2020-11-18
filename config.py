9fdfd1c396875feaa1ee9a93436eaa3d02b3f91a
forza111 <nikitos.ionkin@yandex.ru>
2020-11-18 22:51:39 +0300

    добавление разбиения по страницам

--- config.py
+++ config.py
@@ -12,3 +12,4 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['nikitos.ionkin@yandex.ru']
+    POST_PER_PAGE = 3
