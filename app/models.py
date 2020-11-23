f9347fd9d9055fa86e0612f10ce4536b3c0c6273
forza111 <nikitos.ionkin@yandex.ru>
2020-11-23 16:27:29 +0300

    СОздание формы и функций сброса пароля #3

--- app/models.py
+++ app/models.py
@@ -65,6 +65,7 @@ class User(UserMixin, db.Model):
             {'reset_password': self.id, 'exp' : time() + expires_in },
             app.config['SERET_KEY'], algorithm='HS256'
         ).decode('utf-8')
+
     @staticmethod
     def verify_reset_password_token(token):
         try:
@@ -73,6 +74,7 @@ class User(UserMixin, db.Model):
         except:
             return
         return User.query.get(id)
+
 
 @login.user_loader
 def load_user(id):
--- flood(tests).py
+++
@@ -1,9 +0,0 @@
-from app import db
-from app.models import User,Post
-
-u = User(username='susan', email='susan@example.com')
-u.set_password('mypassword')
-
-u.check_password('anotherpassword')
-
-u.check_password('mypassword')
