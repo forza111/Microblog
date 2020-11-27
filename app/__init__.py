d3b59ec526c22eb2bb6f83ca14a15ed2de2482d2
forza111 <nikitos.ionkin@yandex.ru>
2020-11-27 17:23:32 +0300

    Подключение Bootstrap.

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,7 @@ import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
 from flask_mail import Mail
+from flask_bootstrap import Bootstrap
 
 
 app = Flask(__name__)
@@ -18,6 +19,8 @@ login = LoginManager(app)
 login.login_view = 'login'
 
 mail = Mail(app)
+
+bootstrap = Bootstrap(app)
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
