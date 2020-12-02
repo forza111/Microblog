bb91c3d4b4fa925e183837cfcd4a52dcf79a3723
forza111 <nikitos.ionkin@yandex.ru>
2020-12-03 00:57:20 +0300

    Работа с языками. Flask-babel.#1

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,9 @@ import os
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
-
+from flask_babel import Babel
+from flask import request
+from flask_babel import lazy_gettext as _l
 
 
 
@@ -20,11 +22,12 @@ migrate = Migrate(app, db)
 
 login = LoginManager(app)
 login.login_view = 'login'
-login.login_message = 'Для просмотра данной страницы войдите в свою  учетную запись'
+login.login_message = _l('Please log in to access this page')
 
 mail = Mail(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+babel = Babel(app)
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
@@ -52,5 +55,11 @@ if not app.debug:
 
     app.logger.setLevel(logging.INFO)
     app.logger.info('Microblog startup')
+
+
+@babel.localeselector
+def get_locale():
+    #return request.accept_languages.best_match(app.config['LANGUAGES'])
+    return 'es'
 
 from app import routes, models, errors
