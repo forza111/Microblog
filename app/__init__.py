d3deead91d7930b2dc7f5e0fdc8cd1b5b41bebed
forza111 <nikitos.ionkin@yandex.ru>
2020-12-03 10:40:15 +0300

    Merge

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,8 @@ import os
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_babel import Babel
+from flask import request
 
 
 
@@ -25,6 +27,7 @@ login.login_message = 'Для просмотра данной страницы �
 mail = Mail(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+babel = Babel(app)
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
@@ -52,5 +55,11 @@ if not app.debug:
 
     app.logger.setLevel(logging.INFO)
     app.logger.info('Microblog startup')
+
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
 
 from app import routes, models, errors
