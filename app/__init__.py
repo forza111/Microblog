1aa5a8fc9a955fad2198fdd8855eb188d3d27b51
forza111 <nikitos.ionkin@yandex.ru>
2020-12-07 16:20:38 +0300

    Добавление функции get - locale, переводящая дату и время на выбранный язык.

--- app/__init__.py
+++ app/__init__.py
@@ -60,6 +60,6 @@ if not app.debug:
 @babel.localeselector
 def get_locale():
     #return request.accept_languages.best_match(app.config['LANGUAGES'])
-    return 'en'
+    return 'es'
 
 from app import routes, models, errors
